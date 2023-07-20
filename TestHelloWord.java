96eedb77bb4ee28889aa9c33268621d735bef0d6
hhhqi <2766257531@qq.com>
2023-07-20 19:46:50 +0800

    Test Hello word

---
+++ TestHelloWord.java
@@ -0,0 +1,11 @@
+import java.net.StandardSocketOptions;
+
+/**
+ * @author MiCai
+ * @dateTime 2023/7/20 19:46
+ * @description 
+ */public class TestHelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
