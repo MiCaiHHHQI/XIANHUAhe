f78673418277690a2267ec252c4d7d76a27f2060
hhhqi <2766257531@qq.com>
2023-07-20 19:50:35 +0800

    Test Hello word

---
+++ com/MiCai/Test.java
@@ -0,0 +1,12 @@
+package com.MiCai;
+
+/**
+ * @author MiCai
+ * @dateTime 2023/7/20 19:49
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Success");
+    }
+}
